c9d126bcfaad5aca94b3f341cc355b2304165b04
YuvrajSingh70 <137608935+YuvrajSingh70@users.noreply.github.com>
2023-07-23 20:05:09 +0530

    Create apifeatures.js

---
+++ backend/utils/apifeatures.js
@@ -0,0 +1,52 @@
+class ApiFeatures{
+  constructor(query,queryStr){
+    this.query = query;
+    this.queryStr = queryStr
+  }
+  search(){
+    const keyword = this.queryStr.keyword ? {
+      name:{
+        $regex:this.queryStr.keyword,
+        $options: "i",
+      },
+    }:{}
+
+
+    this.query = this.query.find({...keyword});
+    return this
+  }
+
+  filter(){
+    const queryCopy= {...this.queryStr}
+
+    //removing feild for category
+    const removeFeilds = ["keyword","page","limit"];
+
+    removeFeilds.forEach(key=>delete queryCopy[key]);
+
+
+
+    // filter for price and rating
+
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => '$${key}');
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+
+  }
+
+  pagination(resultPerPage){
+    const currentPage = Number(this.queryStr.page) || 1; // contained in a page timer 1:48:00
+
+    const skip = resultPerPage * (currentPage-1);
+
+    this.query=this.query.limit(resultPerPage).skip(skip);
+    return this;
+  }
+}
+
+
+
+module.exports = ApiFeatures
